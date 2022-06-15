91689348b92895250de6514183ea8497a2a3895a
GRitchie1 <gregor.homeautomation@gmail.com>
2022-06-15 20:15:57 +0100

    Add growing skull

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import spiral from './spiral.svg';
+import skull from './skull.png';
 import './Landing.css';
 
 function Landing() {
@@ -14,6 +15,7 @@ function Landing() {
       <img src={spiral} className={`spiral ${isRotating ? 'spin' : ''}`} alt="logo" />
       <div className="LandingTextContainer">
         <h1 className="Title1">Welcome to The Void</h1>
+        <img src={skull} className={`skull ${isRotating ? 'grow' : ''}`} alt="logo" />
         <button className="EnterButton" onClick={onRotate}>Click here to enter, if you dare!</button>
       </div>
       </header>
