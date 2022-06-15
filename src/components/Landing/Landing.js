f2cfd10dc8028f9c2b963d9800deb9c6461a08b2
GRitchie1 <gregor.homeautomation@gmail.com>
2022-06-15 17:27:49 +0100

    Add spin on button press

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,14 +1,20 @@
+import React from 'react';
 import spiral from './spiral.svg';
-import styles from './Landing.module.css';
+import './Landing.css';
 
 function Landing() {
+  const [isRotating, setIsRotating] = React.useState(false);
+  const onRotate = () => {
+    setIsRotating(true)
+  };
+
   return (
-    <div className={styles.Landing}>
-      <header className={styles.LandingContainer}>
-      <img src={spiral} className={styles.spiral} alt="logo" />
-      <div className={styles.TextContainer}>
-        <h1 className={styles.Title1}>Welcome to The Void</h1>
-        <button className={styles.EnterButton} onClick={()=>{alert('hi')}}>Click here to enter, if you dare!</button>
+    <div className="Landing">
+      <header className="LandingContainer">
+      <img src={spiral} className={`spiral ${isRotating ? 'spin' : ''}`} alt="logo" />
+      <div className="LandingTextContainer">
+        <h1 className="Title1">Welcome to The Void</h1>
+        <button className="EnterButton" onClick={onRotate}>Click here to enter, if you dare!</button>
       </div>
       </header>
     </div>
