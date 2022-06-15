769594a7149c36e8ef1d8654bdce37a45a5df61f
GRitchie1 <gregor.homeautomation@gmail.com>
2022-06-15 16:35:23 +0100

    Create initial page with spiral

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import spiral from './spiral.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={spiral} className="spiral" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
